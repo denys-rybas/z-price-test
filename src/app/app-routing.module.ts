723817ba3805c7be2ef73efcfc182510bfbcb48b
“Dennis.r.ysbm” <android6636@gmail.com>
2020-01-28 15:36:47 +0200

    Base header with routing. Added User and About components. Installed bootstrap and devxtream

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// test
+import {UsersListComponent} from './users-list/users-list.component';
+import {AboutUsComponent} from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersListComponent},
+  {path: 'about', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  UsersListComponent,
+  AboutUsComponent
+];
