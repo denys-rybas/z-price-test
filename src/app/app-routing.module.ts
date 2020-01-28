f41b5b995a2e4bcb2915376953f9772cd5364c3a
“Dennis.r.ysbm” <android6636@gmail.com>
2020-01-28 19:01:22 +0200

    Home page with welcome screen. Changed components structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {UsersListComponent} from './users-list/users-list.component';
-import {AboutUsComponent} from './about-us/about-us.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {UsersListComponent} from './Pages/users-list/users-list.component';
+import {AboutUsComponent} from './Pages/about-us/about-us.component';
+import {HomeComponent} from './Pages/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'users', component: UsersListComponent},
   {path: 'about', component: AboutUsComponent}
 ];
@@ -12,9 +14,11 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
 
 export const routingComponents = [
+  HomeComponent,
   UsersListComponent,
   AboutUsComponent
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FirstScreenComponent } from './Screens/first-screen/first-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    routingComponents
+    routingComponents,
+    FirstScreenComponent,
   ],
   imports: [
     BrowserModule,
